8837167fa4d03f6fa243ec33cc04e4d51416a326
Ojith Adithya <ojithadithya1000@gmail.com>
2026-02-23 16:22:52 +0530

    Clean up main.cpp by removing commented code
    Removed commented-out code and retained the core functionality for reading soil moisture levels.

--- main.cpp
+++ main.cpp
@@ -1,44 +1,5 @@
 #include <Arduino.h>
 
-// // Most ESP32 DevKits have the built-in LED on GPIO 2
-// #define LED_PIN 2
-
-// void setup() {
-//     // Initialize serial communication at 115200 baud
-//     Serial.begin(115200);
-    
-//     // Set the LED pin as an output
-//     pinMode(LED_PIN, OUTPUT);
-    
-//     Serial.println("--- ArchiSRI System Test Starting ---");
-//     Serial.println("If you see this, your USB Driver is working!");
-// }
-
-// void loop() {
-//     // Turn the LED on
-//     digitalWrite(LED_PIN, HIGH);
-//     Serial.println("LED ON");
-//     delay(1000); // Wait for 1 second
-    
-//     // Turn the LED off
-//     digitalWrite(LED_PIN, LOW);
-//     Serial.println("LED OFF");
-//     delay(1000); // Wait for 1 second
-// }
-
-// Capacitive Soil Moisture Sensor Test
-
-//   // Read the analog value (0 to 4095 on ESP32)
-//   int rawValue = analogRead(sensorPin);
-  
-//   // Note: Capacitive sensors read HIGHER values when DRY 
-//   // and LOWER values when WET.
-//   Serial.print("Raw Sensor Value: ");
-//   Serial.println(rawValue);
-
-//   delay(1000); 
-#include <Arduino.h>
-
 // 1. Identify the Pin
 const int sensorPin = 34; // Yellow wire connected to D34
 
@@ -77,86 +38,8 @@ void loop() {
 
 
 
-// #define RE_DE_PIN 4
-
-// // Standard Modbus RTU request for Address 0x01, Read 1 Register starting at 0x0000
-// const byte moistureRequest[] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x0A};
-// byte sensorResponse[7]; 
-
-// void setup() {
-//   Serial.begin(115200);
-//   Serial2.begin(9600, SERIAL_8N1, 16, 17); // Most sensors use 9600 baud
-//   pinMode(RE_DE_PIN, OUTPUT);
-//   digitalWrite(RE_DE_PIN, LOW);
-//   Serial.println("Soil Sensor Initialized...");
-// }
-
-// void loop() {
-//   digitalWrite(RE_DE_PIN, HIGH); // Talk mode
-//   delay(10);
-//   Serial2.write(moistureRequest, sizeof(moistureRequest));
-//   Serial2.flush();
-//   digitalWrite(RE_DE_PIN, LOW); // Listen mode
-
-//   unsigned long startTime = millis();
-//   int byteCount = 0;
-
-//   // Wait up to 500ms for response
-//   while (millis() - startTime < 500) {
-//     if (Serial2.available()) {
-//       sensorResponse[byteCount++] = Serial2.read();
-//     }
-//     if (byteCount == 7) break; 
-//   }
-
-//   if (byteCount == 7) {
-//     // Industrial sensors often return values multiplied by 10
-//     int rawMoisture = (sensorResponse[3] << 8) | sensorResponse[4];
-//     float moisturePercent = rawMoisture * 0.1; 
-    
-//     Serial.print("Soil Moisture: ");
-//     Serial.print(moisturePercent);
-//     Serial.println("%");
-//   } else {
-//     Serial.println("Error: No response from sensor. Check wiring/power.");
-//   }
-
-//   delay(2000);
-// }
-
-
-// // Basic RS485 Communication Test for ESP32
-// #define RE_DE_PIN 4  // Connects to both RE and DE pins
-
-// void setup() {
-//   Serial.begin(115200);          // For Serial Monitor
-//   Serial2.begin(9600, SERIAL_8N1, 16, 17); // RS485 on pins 16(RX) and 17(TX)
-  
-//   pinMode(RE_DE_PIN, OUTPUT);
-//   digitalWrite(RE_DE_PIN, LOW); // Start in "Listening" mode
-  
-//   Serial.println("RS485 Test Initialized...");
-// }
-
-// void loop() {
-//   // Sending a test message out through the RS485 module
-//   digitalWrite(RE_DE_PIN, HIGH); // Switch to "Talking" mode
-//   Serial2.print("Testing RS485...");
-//   delay(10); 
-//   digitalWrite(RE_DE_PIN, LOW);  // Switch back to "Listening" mode
-  
-//   Serial.println("Sent: Testing RS485...");
-  
-//   // If you have a sensor connected, it would reply here
-//   while (Serial2.available()) {
-//     char inChar = (char)Serial2.read();
-//     Serial.print("Received: ");
-//     Serial.println(inChar);
-//   }
-  
-//   delay(2000);
-// }
 
 //task 02
+
 
 
