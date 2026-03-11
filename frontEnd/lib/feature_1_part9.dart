e8f6f730f6df9daaacbc37f75e43b9dadd0342bf
Sithija_Meghana <sithijameghanasenanayaka456@gmail.com>
2026-03-12 00:29:48 +0530

    chore(step): update feature_1_part9.dart

--- frontEnd/lib/feature_1_part9.dart
+++ frontEnd/lib/feature_1_part9.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:archisri_1/feature_1_part10.dart';
+import 'package:archisri_1/feature_1_selections.dart';
 
 //bathroom type selection screen
 
@@ -260,6 +261,7 @@ class _Feature1Part9State extends State<Feature1Part9> {
                             ? () {
                                 // Handle next action
                                 print('Selected rooms: $selectedRooms');
+                                BlueprintSelections.bathroomSelectionsByFloor['second'] = List<String>.from(selectedRooms);
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(builder: (context) => const Feature1Part10()),
