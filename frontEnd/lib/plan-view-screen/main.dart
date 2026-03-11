57867565ae0321f813b553f23592a139819b8f96
Sithija_Meghana <sithijameghanasenanayaka456@gmail.com>
2026-03-11 22:00:49 +0530

    feat(plan-view): add standalone entrypoint

---
+++ frontEnd/lib/plan-view-screen/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'houseplan_designer_screen.dart';
+
+void main() {
+  runApp(const ArchiSriApp());
+}
+
+class ArchiSriApp extends StatelessWidget {
+  const ArchiSriApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ArchiSri',
+      theme: ThemeData(primarySwatch: Colors.orange, useMaterial3: true),
+      home: HouseplanDesignerScreen(),
+    );
+  }
+}
