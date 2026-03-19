76267e79a2a80cf69ab79a3a6a3f8f3972de1070
desanduimanjith-droid <desanduimanjith@gmail.com>
2026-03-19 21:20:40 +0530

    Add files via upload

--- backend/marketPlace/marketPlace.py
+++ backend/marketPlace/marketPlace.py
@@ -1,1 +1,62 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import stripe
+import os
+from dotenv import load_dotenv
 
+
+load_dotenv(dotenv_path=".env")
+
+app = Flask(__name__)
+CORS(app)
+
+# Your Real Key
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
+
+if not stripe.api_key:
+    raise ValueError("Stripe API key not found. Check your .env file.")
+
+# Fixed Warning Logic
+if stripe.api_key.startswith("sk_test_51"):
+    print("WARNING: You are using a placeholder Stripe API key.")
+
+@app.route('/')
+def home():
+    return "Server is Live!"
+
+@app.route('/create-checkout', methods=['POST'])
+def create_checkout():
+    try:
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': 'ArchiSri GS Pro',
+                        'description': 'IoT Soil Moisture & Saltiness Sensor',
+                    },
+                    'unit_amount': 1443, 
+                },
+                'quantity': 1,
+            }],
+            mode='payment',
+            success_url='http://127.0.0.1:5001/success',
+            cancel_url='http://127.0.0.1:5001/cancel',
+        )
+        return jsonify({'url': session.url})
+    except Exception as e:
+        print(f"Error: {e}") # This prints the error in your terminal
+        return jsonify(error=str(e)), 500
+
+@app.route('/success')
+def success():
+    return " Payment Successful!"
+
+@app.route('/cancel')
+def cancel():
+    return " Payment Cancelled!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True,use_reloader =False)
